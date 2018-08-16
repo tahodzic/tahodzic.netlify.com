0fb1bf27113dc47a6b0e268e4b40c5f93b5369de
tahodzic <damir.hodzic@stud.hslu.ch>
2018-08-16 17:10:50 +0200

    Initial commit

---
+++ config/SiteConfig.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Damir Hodzic', // Navigation and Site Title
+  siteTitleAlt: 'Damir Hodzic\'s Blog', // Alternative Site title for SEO
+  siteUrl: 'https://tahodzic.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en',
+  siteDescription: 'A blog about software development', 
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.js
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Yu Gothic',
+  baseFontSize: '18px',
+};
